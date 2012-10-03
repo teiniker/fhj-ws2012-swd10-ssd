c5d3e25b0d526c1eb49ae542f920f0b23791f523
chr.krenn@gmail.com <chr.krenn@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-10-03 23:10:01 +0000

    version

--- SVNExample/SVNExample/src/at/fhj/itm/Constants.java
+++ SVNExample/SVNExample/src/at/fhj/itm/Constants.java
@@ -5,7 +5,7 @@ public class Constants
     /**
      * Application's version number
      */
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     
     /**
      * Date of application building.
