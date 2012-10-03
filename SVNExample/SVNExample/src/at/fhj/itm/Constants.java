5b6771f72c49142c362f90c619b34be14bf2912a
chr.krenn@gmail.com <chr.krenn@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-10-03 23:08:46 +0000

    checking Ws2

---
+++ SVNExample/SVNExample/src/at/fhj/itm/Constants.java
@@ -0,0 +1,14 @@
+package at.fhj.itm;
+
+public class Constants
+{
+    /**
+     * Application's version number
+     */
+    public static final String VERSION = "1.0.7";
+    
+    /**
+     * Date of application building.
+     */
+    public static final String BUILD_DATE = "13. Oktober 2012 20:20:20";
+}
