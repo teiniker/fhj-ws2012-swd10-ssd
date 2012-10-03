2f22311e65812700628f06301aaeae668e7dc065
chr.krenn@gmail.com <chr.krenn@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-10-03 20:48:14 +0000

    Initial Checkin

---
+++ CiExample/trunk/AntExample/src/at/fhj/itm/Constants.java
@@ -0,0 +1,14 @@
+package at.fhj.itm;
+
+public class Constants
+{
+    /**
+     * Application's version number
+     */
+    public static final String VERSION = "3.1.6";
+    
+    /**
+     * Date of application building.
+     */
+    public static final String BUILD_DATE = "20. Oktober 2011 17:36:20";
+}
---
+++ CiExample/trunk/AntExample/templates/Constants.java
@@ -0,0 +1,14 @@
+package at.fhj.itm;
+
+public class Constants
+{
+    /**
+     * Application's version number
+     */
+    public static final String VERSION = "@VERSION@";
+    
+    /**
+     * Date of application building.
+     */
+    public static final String BUILD_DATE = "@BUILD_DATE@";
+}
