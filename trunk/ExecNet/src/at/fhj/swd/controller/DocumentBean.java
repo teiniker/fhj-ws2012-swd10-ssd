68d73827c9b2760ce09c9dc0d9fb4061cca6da3d
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:15:37 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/controller/DocumentBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/DocumentBean.java
@@ -10,90 +10,79 @@ import at.fhj.swd.domain.Community;
 import at.fhj.swd.domain.Document;
 import at.fhj.swd.domain.User;
 
-
 public class DocumentBean {
-	
-	private Date datecreated;
 
-	private String name;
-		
-	private String url;
-	
-	private boolean ispublic = true;
-	private Collection<Community> communities;
-	private IRuntimeContext _rt;
-	private IDataContext<Document> _dc;
-	
-	public DocumentBean() {
-		this._rt = Application.getInstance().getRuntime();
-		this._dc = Application.getInstance().getDocumentContext();
-	}
-	
-	
-	public Date getDateCreated() {
-		return datecreated;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    private Date datecreated;
+    private String name;
+    private String url;
+    private boolean ispublic = true;
 
+    private Collection<Community> communities;
 
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	
-	public Collection<Community> getCommunities() {
-		return communities;
-	}
+    private IRuntimeContext _rt;
+    private IDataContext<Document> _dc;
 
-	public void setCommunities(Collection<Community> communities) {
-		this.communities = communities;
-	}
+    public DocumentBean() {
+        this._rt = Application.getInstance().getRuntime();
+        this._dc = Application.getInstance().getDocumentContext();
+    }
 
-	public boolean isPublic() {
-		return ispublic;
-	}
+    public String addNow() {
+        User _u = _rt.getCurrentUser();
 
-	public void setPublic(boolean ispublic) {
-		this.ispublic = ispublic;
-	}
-	
-	
-	
-	public String addNow() {
-		
-		User _u =_rt.getCurrentUser();
-		
-		Document _new = new Document();
-		_new.setOwner(_u);
-		_new.setPublic(this.isPublic());
-		_new.setUrl(this.getUrl());
-		
-		try {
-			if (_dc.create(_new)) {
-				for (Community c : this.getCommunities())
-				{
-					c.addDocument(_new);
-				}
-				return "document-added";
-			}
-			else {
-				return "documentadding-failed";
-			}
-		} catch (Exception e){
-			e.printStackTrace();
-			return "documentadding-failed";
-		}
-		
-		
-	}
+        Document _new = new Document();
+        _new.setOwner(_u);
+        _new.setPublic(this.isPublic());
+        _new.setUrl(this.getUrl());
 
+        try {
+            if (_dc.create(_new)) {
+                for (Community c : this.getCommunities()) {
+                    c.addDocument(_new);
+                }
+                return "document-added";
+            } else {
+                return "documentadding-failed";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "documentadding-failed";
+        }
+    }
 
+    public Date getDateCreated() {
+        return datecreated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isPublic() {
+        return ispublic;
+    }
+
+    public void setPublic(boolean ispublic) {
+        this.ispublic = ispublic;
+    }
+
+    public Collection<Community> getCommunities() {
+        return communities;
+    }
+
+    public void setCommunities(Collection<Community> communities) {
+        this.communities = communities;
+    }
 }
--- trunk/ExecNet/src/at/fhj/swd/controller/UserBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/UserBean.java
@@ -10,164 +10,146 @@ import at.fhj.swd.application.IRuntimeContext;
 import at.fhj.swd.data.IDataContext;
 import at.fhj.swd.domain.User;
 
-public class UserBean  {
-	
-	private String username;
-	private String oldpassword;
-	private String password;
-	private String email;
-	private String culture = "en";
-	
-	private IDataContext<User> _context;
-	IRuntimeContext _rt;
-	
-	public UserBean(){
-		_rt = Application.getInstance().getRuntime();
-		_context = Application.getInstance().getUserContext();
-	}
+public class UserBean {
 
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getOldpassword() {
-		return oldpassword;
-	}
+    private String username;
+    private String oldpassword;
+    private String password;
+    private String email;
+    private String culture = "en";
 
-	public void setOldpassword(String oldpassword) {
-		this.oldpassword = oldpassword;
-	}
+    private IDataContext<User> _context;
+    private IRuntimeContext _rt;
 
-	/**
-	 * Change the culture of the current user
-	 * @param culture - the two digit culture
-	 * @return navigation path
-	 */
-	public String changeCulture(String culture) {
-		User _newuser = _rt.getCurrentUser();
-		_newuser.setCulture(culture);
-		_rt.setAuthenticated(_context.update(_newuser));
-		return "language-change";
-	}
-	
-	/**
-	 * Tries to login the current user
-	 * @return navigation path
-	 */
-	public String loginNow(){
-		
-		IRuntimeContext _rt = Application.getInstance().getRuntime();
+    public UserBean() {
+        _rt = Application.getInstance().getRuntime();
+        _context = Application.getInstance().getUserContext();
+    }
 
-		this.setPassword(hashSHA1(this.getPassword()));
+    public String changeCulture(String culture) {
+        User _newuser = _rt.getCurrentUser();
+        _newuser.setCulture(culture);
+        _rt.setAuthenticated(_context.update(_newuser));
+        return "language-change";
+    }
 
-		try {
-			String _q = "i._username='" + this.getUsername() + "' and i._password='" + this.getPassword() + "'"; 
-			User _ref;
-			_ref = _context.readOneByQuery(_q,User.class);
-			_ref.authenticate();
-			_rt.setAuthenticated(_ref);
-			return "logged-in";
+    public String loginNow() {
+        IRuntimeContext _rt = Application.getInstance().getRuntime();
+        this.setPassword(hashSHA1(this.getPassword()));
 
-		} catch (Exception e) {
-			return "login-failed";
-		}
+        try {
+            String _q = "i._username='" + this.getUsername() + "' and i._password='" + this.getPassword() + "'";
+            User _ref;
+            _ref = _context.readOneByQuery(_q, User.class);
+            _ref.authenticate();
+            _rt.setAuthenticated(_ref);
+            return "logged-in";
+        } catch (Exception e) {
+            return "login-failed";
+        }
+    }
 
+    public String logoutNow() {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession)ctx.getExternalContext().getSession(true);
+        session.removeAttribute("user");
+        session.invalidate();
+        return "logged-out";
+    }
 
-	}
-	
-	/**
-	 * Loggs out the user
-	 * @return navigation path 
-	 */
-	public String logoutNow(){
-		FacesContext ctx = FacesContext.getCurrentInstance();
-		HttpSession session = (HttpSession)ctx.getExternalContext().getSession(true);
-		session.removeAttribute("user");
-		session.invalidate();
-		return "logged-out";
-	}
-	
-	/**
-	 * Registers the current user to the database
-	 * @return navigation path
-	 */
-	public String registerNow(){
+    public String registerNow() {
+        IRuntimeContext _rt = Application.getInstance().getRuntime();
 
-		IRuntimeContext _rt = Application.getInstance().getRuntime();
-		
-		User _newuser = new User();
-		_newuser.setUsername(this.getUsername());
-		_newuser.setEmail(this.getEmail());
-		_newuser.setAdmin(true);
-		_newuser.setCulture(this.getCulture());
-		_newuser.setPassword(hashSHA1(this.getPassword()));
+        User _newuser = new User();
+        _newuser.setUsername(this.getUsername());
+        _newuser.setEmail(this.getEmail());
+        _newuser.setAdmin(true);
+        _newuser.setCulture(this.getCulture());
+        _newuser.setPassword(hashSHA1(this.getPassword()));
 
-		try {
-			if(_context.create(_newuser)){
-				_newuser.authenticate();
-				_rt.setAuthenticated(_newuser);
-				return "logged-in";
-			} else {
-				return "register-failed";
-			}
-		} catch (Exception e){			
-			return "register-failed";
-		}
-	}
-	
-	public String editNow(){
-		try {		
-			User _newuser = _rt.getCurrentUser();
-			
-			if (hashSHA1(this.getPassword()).equals(_newuser.getPassword()))
-					return "edit-failed";
-			_newuser.setUsername(this.getUsername());
-			_newuser.setEmail(this.getEmail());
-			_newuser.setAdmin(true);
-			_newuser.setCulture(this.getCulture());
-			_newuser.setPassword(hashSHA1(this.getPassword()));
+        try {
+            if (_context.create(_newuser)) {
+                _newuser.authenticate();
+                _rt.setAuthenticated(_newuser);
+                return "logged-in";
+            } else {
+                return "register-failed";
+            }
+        } catch (Exception e) {
+            return "register-failed";
+        }
+    }
 
-			_rt.setAuthenticated(_context.update(_newuser));
-			return "user-edited";
-		} catch (Exception e){			
-			return "edit-failed";
-		}
-	}
-	
+    public String editNow() {
+        try {
+            User _newuser = _rt.getCurrentUser();
 
-	private String hashSHA1(String password){
-		try {
-			MessageDigest cript;
-			cript = MessageDigest.getInstance("SHA-1");
-		   	cript.reset();
-        	cript.update(password.getBytes());
-        	password = new String(cript.digest());
-		} catch (Exception e) {
-			// assert plain text password
-		}
-		return password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getCulture() {
-		return culture;
-	}
-	public void setCulture(String culture) {
-		this.culture = culture;
-	}
+            if (hashSHA1(this.getPassword()).equals(_newuser.getPassword())) {
+                return "edit-failed";
+            }
+            _newuser.setUsername(this.getUsername());
+            _newuser.setEmail(this.getEmail());
+            _newuser.setAdmin(true);
+            _newuser.setCulture(this.getCulture());
+            _newuser.setPassword(hashSHA1(this.getPassword()));
 
+            _rt.setAuthenticated(_context.update(_newuser));
+            return "user-edited";
+        } catch (Exception e) {
+            return "edit-failed";
+        }
+    }
 
+    private String hashSHA1(String password) {
+        try {
+            MessageDigest cript;
+            cript = MessageDigest.getInstance("SHA-1");
+            cript.reset();
+            cript.update(password.getBytes());
+            password = new String(cript.digest());
+        } catch (Exception e) {
+            // assert plain text password
+        }
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOldpassword() {
+        return oldpassword;
+    }
+
+    public void setOldpassword(String oldpassword) {
+        this.oldpassword = oldpassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCulture() {
+        return culture;
+    }
+
+    public void setCulture(String culture) {
+        this.culture = culture;
+    }
 }
