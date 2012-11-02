5d8e1ab9ec8bea9f1bea195f30be794e7f5fd9b3
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:25:23 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/domain/ActivityStream.java
+++ trunk/ExecNet/src/at/fhj/swd/domain/ActivityStream.java
@@ -1,56 +1,15 @@
 package at.fhj.swd.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
 
 public class ActivityStream {
-	
-	private IRuntimeContext _runtime;
-	
 
-	public ActivityStream(){
-		Application _app = Application.getInstance();
-		_runtime = _app.getRuntime();
-	}
-	
-/*
-	public List<Post> getAllPosts(){
-		User _user = _runtime.getCurrentUser();
-		List<Post> _result = new ArrayList<Post>();
-		if (_user.isLoggedIn()){
-			_result = _pcontext.getPostsByUser(_user, false);
-		}
-		return _result;
-	}
-	
-	public List<Post> getPublicPosts(){
-		User _user = _runtime.getCurrentUser();
-		List<Post> _result = new ArrayList<Post>();
-		if (_user.isLoggedIn()){
-			_result = _pcontext.getPostsByUser(_user, true);
-		}
-		return _result;
-	}
-	
-	public List<Post> getPosts(Community community){
-		User _user = _runtime.getCurrentUser();
-		List<Post> _result = new ArrayList<Post>();
-		if (_user.isLoggedIn() && _user.getCommunities().contains(community)){
-			_result = _pcontext.getPostsByCommunity(community);
-		}
-		return _result;
-	}
+    private IRuntimeContext _runtime;
 
+    public ActivityStream() {
+        Application _app = Application.getInstance();
+        _runtime = _app.getRuntime();
+    }
 
-	public Community getCommunity() {
-		return _community;
-	}
-
-
-	public void setCommunity(Community community) {
-		this._community = community;
-	} */
 }
