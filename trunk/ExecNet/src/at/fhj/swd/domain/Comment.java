ad5d3d007402b2cfc3e9a1ff179b1684543fba5a
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:54:27 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/domain/Comment.java
+++ trunk/ExecNet/src/at/fhj/swd/domain/Comment.java
@@ -15,103 +15,120 @@ import javax.persistence.TemporalType;
 
 @Entity(name = "Comment")
 @Table(name = "tbl_Comment")
-public class Comment {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "F_DATE", nullable = false)
-	@Temporal(TemporalType.DATE)
-	private Date date;
-	@Column(name = "F_ENTRY", nullable = false)
-	private String entry;
-	
-	@ManyToOne
-	private Post post;
-		
-	@ManyToOne
-	private User user;
-	
-	
-	public Date getDate() {
-		return date;
-	}
-	
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Post getPost() {
-		return post;
-	}
+public class Comment implements IEntity {
 
-	public void setPost(Post post) {
-		this.post = post;
-	}
+    public static final String ENTITY_NAME = "Comment";
 
-	public String getEntry() {
-		return entry;
-	}
-	public void setEntry(String entry) {
-		this.entry = entry;
-	}
-	
-	
-	public String toString() {
-		return "userId = " + user.getUsername() + ", date = " + date + ": entry = " + entry; 
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + ((entry == null) ? 0 : entry.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + (int) (user.getId() ^ (user.getId() >>> 32));
-		return result;
-	}
+    @Column(name = "F_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date date;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		if (date == null) {
-			if (other.date != null)
-				return false;
-		} else if (!date.equals(other.date))
-			return false;
-		if (entry == null) {
-			if (other.entry != null)
-				return false;
-		} else if (!entry.equals(other.entry))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (user.getId() != other.user.getId())
-			return false;
-		return true;
-	}
-	
+    @Column(name = "F_ENTRY", nullable = false)
+    private String entry;
 
-	public String getTaskDate() {
-		SimpleDateFormat dateformatDDMMYYYY = new SimpleDateFormat("dd.MM.yyyy");
-		StringBuilder formatedDate = new StringBuilder( dateformatDDMMYYYY.format( date ));
-		return formatedDate.toString();
-	}
+    @ManyToOne
+    private Post post;
 
+    @ManyToOne
+    private User user;
+
+    public Comment() {
+    }
+
+    public String getTaskDate() {
+        SimpleDateFormat dateformatDDMMYYYY = new SimpleDateFormat("dd.MM.yyyy");
+        StringBuilder formatedDate = new StringBuilder(dateformatDDMMYYYY.format(date));
+        return formatedDate.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "userId = " + user.getUsername() + ", date = " + date + ": entry = " + entry;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Comment other = (Comment)obj;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (entry == null) {
+            if (other.entry != null)
+                return false;
+        } else if (!entry.equals(other.entry))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (user.getId() != other.user.getId())
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((entry == null) ? 0 : entry.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + (int)(user.getId() ^ (user.getId() >>> 32));
+        return result;
+    }
+
+    @Override
+    public String getEntityName() {
+        return ENTITY_NAME;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
