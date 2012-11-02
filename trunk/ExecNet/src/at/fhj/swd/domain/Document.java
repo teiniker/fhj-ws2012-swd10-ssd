16100e1a1aacb6f106a1875336beefae35f14a93
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 10:18:37 +0000

    Added Object Methods.

--- trunk/ExecNet/src/at/fhj/swd/domain/Document.java
+++ trunk/ExecNet/src/at/fhj/swd/domain/Document.java
@@ -91,6 +91,37 @@ public class Document implements IEntity {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Document other = (Document)obj;
+        if (datecreated == null) {
+            if (other.datecreated != null)
+                return false;
+        } else if (!datecreated.equals(other.datecreated))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((datecreated == null) ? 0 : datecreated.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
     public String getEntityName() {
         return Document.ENTITY_NAME;
     }
