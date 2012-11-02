5e5daee07c9e8791f65bb5931ac782ddf8d0007c
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:56:52 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/domain/ActivityStream.java
+++ trunk/ExecNet/src/at/fhj/swd/domain/ActivityStream.java
@@ -3,13 +3,27 @@ package at.fhj.swd.domain;
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
 
-public class ActivityStream {
+public class ActivityStream implements IEntity {
 
     private IRuntimeContext _runtime;
 
     public ActivityStream() {
         Application _app = Application.getInstance();
         _runtime = _app.getRuntime();
+    }
+
+    // TODO implement ActivityStream.
+
+    @Override
+    public String getEntityName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long getId() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
