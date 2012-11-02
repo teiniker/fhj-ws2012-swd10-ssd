5c4a5b4230d8a00ee3cf0545ef8783b7ffa6f46f
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:17:50 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/data/DBContext.java
+++ trunk/ExecNet/src/at/fhj/swd/data/DBContext.java
@@ -13,147 +13,118 @@ import org.apache.log4j.Logger;
 import at.fhj.swd.domain.IEntity;
 
 public class DBContext<T> implements IDataContext<T> {
-	
-	protected static final String PERSISTENCE_UNIT_NAME = "ExecNet";
-	protected static EntityManagerFactory _factory;
-	protected static final Logger logger = Logger.getLogger(DBContext.class.getName());
-		
-	public DBContext(){
-		_factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME); 
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#create(T)
-	 */
-	@Override
-	public boolean create  (T item){
-		boolean result = false;
-		
-		EntityManager _em = _factory.createEntityManager();
-		
-		try {
-			_em.getTransaction().begin();
-			_em.persist(item);
-			_em.getTransaction().commit();
-			result = true;
-		} catch (Exception ex) {
-			_em.getTransaction().rollback();
-			logger.error(ex.getMessage(), ex);
-			result = false;
-		} finally {
-			_em.close();
-		}
-		
-		return result;
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#readOneByQuery(java.lang.String, java.lang.Class)
-	 */
-	@Override
-	@SuppressWarnings("unchecked")
-	public T readOneByQuery (String query, Class<T> c) throws Exception{
-		EntityManager _em = _factory.createEntityManager();
 
-		T _x = (T) c.newInstance();
-		String _s =  ((IEntity)_x).getEntityName();
-		
-		Query _q = _em.createQuery("select i from " + _s + " i where " +  query);
-			//u._username =?1").setParameter(1, user.getUsername());
-		
-		try {
-			T _entry = (T) _q.getSingleResult();
-			return _entry;
-		} catch (NoResultException e) {
-			logger.debug("No results found");
-			return null;
-		} finally {
-			_em.close();
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#readOne(long, java.lang.Class)
-	 */
-	@Override
-	public T readOne (long id, Class<T> c) {
-		EntityManager _em = _factory.createEntityManager();
-		
-		try {
-			T _x = (T) c.newInstance();
-			_x = _em.find(c, id);
-			return _x;
-		}  catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			return null;
-		} 
-		finally {
-			_em.close();
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#readAll(java.lang.Class)
-	 */
-	@Override
-	@SuppressWarnings("unchecked")
-	public Collection<T> readAll(Class<T> c){
-		EntityManager _em = _factory.createEntityManager();
-		 
-		try {
-			T _x = (T) c.newInstance();
-			IEntity _i = (IEntity)_x;
-			String _s =  _i.getEntityName();
-			Query query = _em.createQuery("select i from " + _s + " i");
-			Collection<T> result = query.getResultList();
-			return result;
-		} catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			return null;
-		}  finally {
-			_em.close();
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#update(T)
-	 */
-	@Override
-	public T update (T item){
-		EntityManager _em = _factory.createEntityManager();
-		try {
-			_em.getTransaction().begin();
-			item = _em.merge(item);
-			_em.getTransaction().commit();
-			}catch (Exception ex) {
-				logger.error(ex.getMessage(), ex);
-		} finally {
-			_em.close();
-		}
-		return item;
-	}
-	
-	/* (non-Javadoc)
-	 * @see at.fhj.swd.data.IDataContext#delete(T)
-	 */
-	@Override
-	public boolean delete (T item){
-		EntityManager _em = _factory.createEntityManager();
-		boolean result = false;
-		try {
-			_em.getTransaction().begin();
-			T target = _em.merge(item);
-			_em.remove(target);
-			_em.getTransaction().commit();
-			result = true;
-		} catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			result = false;
-		} finally {
-			_em.close();
-		}
-					
-		return result;
-	}
+    protected static final String PERSISTENCE_UNIT_NAME = "ExecNet";
+    protected static EntityManagerFactory _factory;
+    protected static final Logger logger = Logger.getLogger(DBContext.class.getName());
+
+    public DBContext() {
+        _factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
+
+    @Override
+    public boolean create(T item) {
+        boolean result = false;
+        EntityManager _em = _factory.createEntityManager();
+        try {
+            _em.getTransaction().begin();
+            _em.persist(item);
+            _em.getTransaction().commit();
+            result = true;
+        } catch (Exception ex) {
+            _em.getTransaction().rollback();
+            logger.error(ex.getMessage(), ex);
+            result = false;
+        } finally {
+            _em.close();
+        }
+        return result;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T readOneByQuery(String query, Class<T> c) throws Exception {
+        EntityManager _em = _factory.createEntityManager();
+        T _x = (T)c.newInstance();
+        String _s = ((IEntity)_x).getEntityName();
+        Query _q = _em.createQuery("select i from " + _s + " i where " + query);
+        try {
+            T _entry = (T)_q.getSingleResult();
+            return _entry;
+        } catch (NoResultException e) {
+            logger.debug("No results found");
+            return null;
+        } finally {
+            _em.close();
+        }
+    }
+
+    @Override
+    public T readOne(long id, Class<T> c) {
+        EntityManager _em = _factory.createEntityManager();
+        try {
+            T _x = (T)c.newInstance();
+            _x = _em.find(c, id);
+            return _x;
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return null;
+        } finally {
+            _em.close();
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<T> readAll(Class<T> c) {
+        EntityManager _em = _factory.createEntityManager();
+        try {
+            T _x = (T)c.newInstance();
+            IEntity _i = (IEntity)_x;
+            String _s = _i.getEntityName();
+            Query query = _em.createQuery("select i from " + _s + " i");
+            Collection<T> result = query.getResultList();
+            return result;
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return null;
+        } finally {
+            _em.close();
+        }
+    }
+
+    @Override
+    public T update(T item) {
+        EntityManager _em = _factory.createEntityManager();
+        try {
+            _em.getTransaction().begin();
+            item = _em.merge(item);
+            _em.getTransaction().commit();
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        } finally {
+            _em.close();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean delete(T item) {
+        EntityManager _em = _factory.createEntityManager();
+        boolean result = false;
+        try {
+            _em.getTransaction().begin();
+            T target = _em.merge(item);
+            _em.remove(target);
+            _em.getTransaction().commit();
+            result = true;
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            result = false;
+        } finally {
+            _em.close();
+        }
+        return result;
+    }
 
 }
--- trunk/ExecNet/src/at/fhj/swd/data/IDataContext.java
+++ trunk/ExecNet/src/at/fhj/swd/data/IDataContext.java
@@ -4,16 +4,16 @@ import java.util.Collection;
 
 public interface IDataContext<T> {
 
-	public abstract boolean create(T item);
+    public abstract boolean create(T item);
 
-	public abstract T readOneByQuery(String query, Class<T> c) throws Exception;
+    public abstract T readOneByQuery(String query, Class<T> c) throws Exception;
 
-	public abstract T readOne(long id, Class<T> c);
+    public abstract T readOne(long id, Class<T> c);
 
-	public abstract Collection<T> readAll(Class<T> c);
+    public abstract Collection<T> readAll(Class<T> c);
 
-	public abstract T update(T item);
+    public abstract T update(T item);
 
-	public abstract boolean delete(T item);
+    public abstract boolean delete(T item);
 
 }
