306139ab386ed19840cd023e5f6709e1a576629f
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-02 09:19:01 +0000

    Code Formatting.

--- trunk/ExecNet/src/at/fhj/swd/utils/NearCache.java
+++ trunk/ExecNet/src/at/fhj/swd/utils/NearCache.java
@@ -3,27 +3,26 @@ package at.fhj.swd.utils;
 import java.util.Date;
 
 public class NearCache<T> {
-	
-	private Date _updated;
-	private T _item;
-	private int _ttl;
 
-	public NearCache(int ttl){
-		_ttl = ttl;
-	}
-	
-	public void  put(T item){
-		_item = item;
-		_updated = new Date();
-	}
-	
-	public T get(){
-		Date now = new Date();
-		if ( _updated.getTime() + _ttl * 1000 < now.getTime() ){
-			_item = null;
-		}
-		
-		return _item;
-		}
+    private Date _updated;
+    private T _item;
+    private int _ttl;
+
+    public NearCache(int ttl) {
+        _ttl = ttl;
+    }
+
+    public void put(T item) {
+        _item = item;
+        _updated = new Date();
+    }
+
+    public T get() {
+        Date now = new Date();
+        if (_updated.getTime() + _ttl * 1000 < now.getTime()) {
+            _item = null;
+        }
+        return _item;
+    }
 
 }
