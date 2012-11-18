c8aa17f3faf47c15679aee6516326dd6f4f4ce01
jan.moser.swd10 <jan.moser.swd10@gmail.com>
2012-11-18 03:35:15 +0000

    Moser Jan - added FileUpload 			UploadDirectorySetting in Web.xml

--- ExecNet/src/at/fhj/swd/controller/DocumentBean.java
+++ ExecNet/src/at/fhj/swd/controller/DocumentBean.java
@@ -1,7 +1,17 @@
 package at.fhj.swd.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
 
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
@@ -12,12 +22,12 @@ import at.fhj.swd.domain.User;
 
 public class DocumentBean {
 
-    private Date datecreated;
-    private String name;
-    private String url;
-    private boolean ispublic = true;
+    private Date _datecreated;
+    private String _name;
+    private String _url;
+    private boolean _ispublic = true;
 
-    private Collection<Community> communities;
+    private Collection<Community> _communities;
 
     private IRuntimeContext _rt;
     private IDataContext<Document> _dc;
@@ -31,15 +41,20 @@ public class DocumentBean {
         User _u = _rt.getCurrentUser();
 
         Document _new = new Document();
-        _new.setOwner(_u);
+        _new.setName(this.getName());
+        _new.setDateCreated(this.getDateCreated());
         _new.setPublic(this.isPublic());
         _new.setUrl(this.getUrl());
+        _new.setOwner(_u);
+
 
         try {
             if (_dc.create(_new)) {
-                for (Community c : this.getCommunities()) {
-                    c.addDocument(_new);
-                }
+                /*
+                 * for (Community c : this.getCommunities()) {
+                 * c.addDocument(_new);
+                 * }
+                 */
                 return "document-added";
             } else {
                 return "documentadding-failed";
@@ -50,39 +65,94 @@ public class DocumentBean {
         }
     }
 
+    public Collection<Document> getAll() {
+        try {
+            return _dc.readAll(Document.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public Date getDateCreated() {
-        return datecreated;
+        return _datecreated;
+    }
+
+    public void setDateCreated(Date _datecreated) {
+        this._datecreated = _datecreated;
     }
 
     public String getName() {
-        return name;
+        return _name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this._name = name;
     }
 
     public String getUrl() {
-        return url;
+        return _url;
     }
 
     public void setUrl(String url) {
-        this.url = url;
+        this._url = url;
     }
 
     public boolean isPublic() {
-        return ispublic;
+        return _ispublic;
     }
 
     public void setPublic(boolean ispublic) {
-        this.ispublic = ispublic;
+        this._ispublic = ispublic;
     }
 
     public Collection<Community> getCommunities() {
-        return communities;
+        return _communities;
     }
 
     public void setCommunities(Collection<Community> communities) {
-        this.communities = communities;
+        this._communities = communities;
+    }
+
+    public void upload(FileUploadEvent event) {
+
+        setName(event.getFile().getFileName());
+        setUrl(FacesContext.getCurrentInstance().getExternalContext().getInitParameter("uploadDirectory")
+            + event.getFile().getFileName());
+        Date date = Calendar.getInstance().getTime();
+        setDateCreated(date);
+
+
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+            addNow();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void copyFile(String fileName, InputStream in) {
+
+        try {
+
+
+            // write the inputStream to a FileOutputStream
+            OutputStream out = new FileOutputStream(new File(_url));
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+            in.close();
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
