ff47cf596856e9ee02cc1d995eaecf33697c78d2
jan.moser.swd10@gmail.com <jan.moser.swd10@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-11-22 20:44:13 +0000

    Moser Jan - DocumentsBeans added file deletion 		  - fixed file duplication issue

--- trunk/ExecNet/src/at/fhj/swd/controller/DocumentBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/DocumentBean.java
@@ -11,6 +11,7 @@ import java.util.Date;
 
 import javax.faces.context.FacesContext;
 
+import org.apache.log4j.Logger;
 import org.primefaces.event.FileUploadEvent;
 
 import at.fhj.swd.application.Application;
@@ -28,47 +29,22 @@ public class DocumentBean {
     private boolean _ispublic = true;
 
     private Collection<Community> _communities;
-
     private IRuntimeContext _rt;
     private IDataContext<Document> _dc;
+
+    protected static final Logger logger = Logger.getLogger(DocumentBean.class.getName());
 
     public DocumentBean() {
         this._rt = Application.getInstance().getRuntime();
         this._dc = Application.getInstance().getDocumentContext();
     }
 
-    public String addNow() {
-        User _u = _rt.getCurrentUser();
-
-        Document _new = new Document();
-        _new.setName(this.getName());
-        _new.setDateCreated(this.getDateCreated());
-        _new.setPublic(this.isPublic());
-        _new.setUrl(this.getUrl());
-        _new.setOwner(_u);
-
-
-        try {
-            if (_dc.create(_new)) {
-                /*
-                 * for (Community c : this.getCommunities()) {
-                 * c.addDocument(_new);
-                 * }
-                 */
-                return "document-added";
-            } else {
-                return "documentadding-failed";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "documentadding-failed";
-        }
-    }
 
     public Collection<Document> getAll() {
         try {
             return _dc.readAll(Document.class);
         } catch (Exception e) {
+            logger.error(e.getMessage(), e);
             return null;
         }
     }
@@ -104,13 +80,14 @@ public class DocumentBean {
     public Boolean delete(Long id) {
         try {
             Document d = _dc.readOne(id, Document.class);
-            if (_dc.delete(d)) {
+            if (_dc.delete(d) && deleteFile(d.getUrl())) {
+
                 return true;
             } else {
                 return false;
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
             return false;
         }
     }
@@ -129,23 +106,44 @@ public class DocumentBean {
 
     public void upload(FileUploadEvent event) {
 
+        User _u = _rt.getCurrentUser();
         setName(event.getFile().getFileName());
-        setUrl(FacesContext.getCurrentInstance().getExternalContext().getInitParameter("uploadDirectory")
-            + event.getFile().getFileName());
-        Date date = Calendar.getInstance().getTime();
-        setDateCreated(date);
-
+        // set empty url -> filename = Document.id
+        setUrl("");
+        Date CreationTime = Calendar.getInstance().getTime();
+        setDateCreated(CreationTime);
 
         try {
-            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
-            addNow();
-            // RequestContext requestContext = RequestContext.getCurrentInstance();
-            // requestContext.update("@all");
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            Document _new = new Document();
+            _new.setName(this.getName());
+            _new.setDateCreated(this.getDateCreated());
+            _new.setPublic(this.isPublic());
+            _new.setUrl(this.getUrl());
+            _new.setOwner(_u);
+            _dc.create(_new);
+
+            // getting file extension
+            String Ext = "";
+            String[] TempArray = this.getName().split("\\.");
+            if (TempArray.length > 1) {
+                Ext = "." + TempArray[TempArray.length - 1];
+            }
+
+            // setting url with Document.id as filename
+            setUrl(FacesContext.getCurrentInstance().getExternalContext().getInitParameter("uploadDirectory")
+                + _new.getId().toString() + Ext);
+
+
+            _new.setUrl(this.getUrl());
+            _new = _dc.update(_new);
+
+
+            copyFile(this.getName(), event.getFile().getInputstream());
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
-
-
     }
 
     public void copyFile(String fileName, InputStream in) {
@@ -168,7 +166,22 @@ public class DocumentBean {
             out.close();
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            logger.error(e.getMessage(), e);
         }
     }
+
+    public boolean deleteFile(String file) {
+        File f = new File(file);
+        boolean bRet = f.delete();
+
+        if (bRet) {
+            logger.info("file deleted");
+        } else {
+            logger.error("file deletion failed");
+        }
+
+        return bRet;
+
+    }
+
 }
