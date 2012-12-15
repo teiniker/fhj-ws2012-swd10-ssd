e47b6b9d61f2689a937fb8ad6eab440955c441d6
scotfox.SF81 <scotfox.SF81@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-12-15 19:25:46 +0000

    FuStef: Erweiterung IDataContext für Collection mit Query

--- ExecNet/src/at/fhj/swd/data/IDataContext.java
+++ ExecNet/src/at/fhj/swd/data/IDataContext.java
@@ -3,18 +3,20 @@ package at.fhj.swd.data;
 import java.util.Collection;
 
 public interface IDataContext<T> {
-	
-	public abstract void beginTransaction();
-	
-	public abstract void commitTransaction();
-	
-	public abstract void rollbackTransaction();
+
+    public abstract void beginTransaction();
+
+    public abstract void commitTransaction();
+
+    public abstract void rollbackTransaction();
 
     public abstract boolean create(T item);
 
     public abstract T readOneByQuery(String query, Class<T> c) throws Exception;
 
     public abstract T readOne(long id, Class<T> c);
+
+    public abstract Collection<T> readByQuery(String query, Class<T> c) throws Exception;
 
     public abstract Collection<T> readAll(Class<T> c);
 
