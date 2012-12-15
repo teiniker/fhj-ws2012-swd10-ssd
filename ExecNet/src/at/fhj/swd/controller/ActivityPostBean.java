8e36b47840365e84429e25a4587a9d337e2e507b
georgmoik <georgmoik@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-12-15 17:07:26 +0000

    georg: activityStream

--- ExecNet/src/at/fhj/swd/controller/ActivityPostBean.java
+++ ExecNet/src/at/fhj/swd/controller/ActivityPostBean.java
@@ -1,77 +1,33 @@
 package at.fhj.swd.controller;
 
 import java.util.Collection;
-import java.util.Date;
 
-import at.fhj.swd.application.Application;
-import at.fhj.swd.application.IRuntimeContext;
-import at.fhj.swd.data.IDataContext;
+import at.fhj.swd.business.ActivityBO;
 import at.fhj.swd.domain.Post;
-import at.fhj.swd.domain.User;
 
 public class ActivityPostBean {
 
-
     private String entry;
-    // private boolean ispublic = true
 
-    private IDataContext<Post> _pc;
-    private IRuntimeContext _rc;
-
+    private ActivityBO _bo;
 
     public ActivityPostBean() {
-        this._pc = Application.getInstance().getPostContext();
-        this._rc = Application.getInstance().getRuntime();
+        this._bo = new ActivityBO();
     }
 
     public String addNow() {
         System.out.println("Activity addNow");
-        User _u = _rc.getCurrentUser();
-
-        Post _new = new Post();
-        _new.setEntry(this.getEntry());
-        _new.setPinboard(_u);
-        _new.setAuthor(_u);
-        _new.setDate(new Date());
-        _new.setActivityEntry(true);
-        _u.addPinPost(_new);
-
-        try {
-            if (_pc.create(_new)) {
-                return "post-added";
-            } else {
-                return "post-failed";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "post-failed";
-        }
+        return _bo.add(entry) ? "post-added" : "post-failed";
     }
 
-    public Boolean delete(Long id) {
-        try {
-            Post p = _pc.readOne(id, Post.class);
-            if (_pc.delete(p)) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
+    public String delete(Long id) {
+        System.out.println("Activity delete");
+        return _bo.delete(id) ? "actitity_deleted" : "acitivity_deleted_failed";
     }
 
     public Collection<Post> getAll() {
-        try {
-
-            return _pc.readAll(Post.class);
-        } catch (Exception e) {
-            return null;
-        }
+        return _bo.getAll();
     }
-
 
     public String getEntry() {
         return entry;
@@ -80,6 +36,4 @@ public class ActivityPostBean {
     public void setEntry(String entry) {
         this.entry = entry;
     }
-
-
 }
