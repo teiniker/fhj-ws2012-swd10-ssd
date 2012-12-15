20df2675e789c6a948a57e6ad7e03fbe0676190a
t.netconomy <t.netconomy@gmail.com>
2012-12-15 17:11:20 +0000

    Fertigstellung CommentBO und Einbindung widget in activitystream

--- ExecNet/src/at/fhj/swd/controller/CommentBean.java
+++ ExecNet/src/at/fhj/swd/controller/CommentBean.java
@@ -1,9 +1,13 @@
 package at.fhj.swd.controller;
 
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
+import at.fhj.swd.business.CommentBO;
 import at.fhj.swd.data.IDataContext;
 import at.fhj.swd.domain.Comment;
 import at.fhj.swd.domain.Post;
@@ -12,6 +16,8 @@ import at.fhj.swd.domain.User;
 public class CommentBean {
 
     private String entry;
+    private ArrayList<Comment> commentList;
+    private CommentBO _bo;
 
     private IRuntimeContext _rt;
     private IDataContext<Comment> _cc;
@@ -19,43 +25,28 @@ public class CommentBean {
     private IDataContext<Post> _pc;
 
     public CommentBean() {
+        _bo = new CommentBO();
         this._rt = Application.getInstance().getRuntime();
         this._cc = Application.getInstance().getCommentContext();
         //Zwischenlösung
         this._pc = Application.getInstance().getPostContext();
+        
+    
        
         
     }
+    
+    /*public List<Comments> getComments(int id_post){
+        this._rt = Application.getInstance().getRuntime();
+        this._cc = Application.getInstance().getCommentContext();
+        
+        return _cc.getComments();
+       
+        
+    } */
 
-    public String addNow() {
-        System.out.println("ADD NOW");
-        
-        User _u = _rt.getCurrentUser();
-        //Dumy Post
-        Post _p = new Post();
-        _p.setDate(new Date());
-        _p.setEntry(this.getEntry());
-        _p.setActivityEntry(true);
-        _p.setAuthor(_u);
-        
-        
-        Comment _new = new Comment();
-        _new.setDate(new Date());
-        _new.setEntry(this.getEntry());
-        _new.setPost(_p);
-        _new.setUser(_u);
-
-        try {
-            if (_pc.create(_p)&&_cc.create(_new)) {
-                _p.addComment(_new);
-                
-                return "comment-added";
-            } else {
-                return "commentadding-failed";
-            }
-        } catch (Exception e) {
-            return "commentadding-failed";
-        }
+    public String addNow(int id_post) {
+       return (_bo.add(id_post,this.getEntry()))?"comment-added":"commentadding-failed";
     }
 
     public String getEntry() {
