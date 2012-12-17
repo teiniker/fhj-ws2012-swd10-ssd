3c5055b647bf145206bfd61e1a693c9fc9a747a8
Eibegger <Eibegger@gmail.com>
2012-12-17 22:30:32 +0000

    Unittest EmailTest hinzugefügt - derzeit nur Dummy-Implementierung, damit CruiseControl und Cobertura was zu testen hat

---
+++ ExecNet/test/at/fhj/swd/controller/EmailTest.java
@@ -0,0 +1,13 @@
+package at.fhj.swd.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EmailTest {
+	
+	@Test
+	public void sendEmail(){
+		assertEquals(1,1);
+	}
+}
