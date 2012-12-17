b64438acc7296713af2d2bfe34def3b9ac8f9350
georgmoik <georgmoik@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-12-17 22:16:40 +0000

    Activities and Comments

--- trunk/ExecNet/src/at/fhj/swd/business/CommentBO.java
+++ trunk/ExecNet/src/at/fhj/swd/business/CommentBO.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
-import at.fhj.swd.controller.CommentBean;
 import at.fhj.swd.data.IDataContext;
 import at.fhj.swd.domain.Comment;
 import at.fhj.swd.domain.Post;
@@ -12,43 +11,34 @@ import at.fhj.swd.domain.User;
 
 
 public class CommentBO extends ABusinessObject {
-    
+
     private IDataContext<Comment> _context;
-    private IDataContext<Post> _postcontext;
     private IRuntimeContext _runtimecontext;
-    private CommentBean _cb;
 
     public CommentBO() {
         super();
         this._context = Application.getInstance().getCommentContext();
-        this._postcontext = Application.getInstance().getPostContext();
         this._runtimecontext = Application.getInstance().getRuntime();
     }
 
-    public boolean add(int id_post, String entry){
+    public boolean add(Post p, String entry) {
         User _u = _runtimecontext.getCurrentUser();
-        _cb = new CommentBean();
- 
-        Post _p = _postcontext.readOne(id_post, Post.class);
-     
-        
+
         Comment _new = new Comment();
         _new.setDate(new Date());
         _new.setEntry(entry);
-        _new.setPost(_p);
+        _new.setPost(p);
         _new.setUser(_u);
-        
+
         try {
-            if (_postcontext.create(_p)&&_context.create(_new)) {
-                _p.addComment(_new);
-                
+            if (_context.create(_new)) {
+                p.addComment(_new);
                 return true;
             } else {
                 return false;
             }
-         
-        }
-        catch (Exception e) {
+
+        } catch (Exception e) {
             logger.error(e);
             return false;
         }
--- trunk/ExecNet/src/at/fhj/swd/controller/ActivityPostBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/ActivityPostBean.java
@@ -3,12 +3,12 @@ package at.fhj.swd.controller;
 import java.util.Collection;
 
 import at.fhj.swd.business.ActivityBO;
+import at.fhj.swd.domain.Community;
 import at.fhj.swd.domain.Post;
 
 public class ActivityPostBean {
 
     private String entry;
-    private boolean isPublic;
     private ActivityBO _bo;
 
     public ActivityPostBean() {
@@ -17,24 +17,39 @@ public class ActivityPostBean {
 
     public Nav addNow() {
         System.out.println("Activity addNow");
-        return (_bo.add(entry, isPublic)) ? Nav.activity_add : Nav.FAIL;
+        if (_bo.add(entry)) {
+            return Nav.activity_add;
+        } else {
+            ThreadLocals.setErrorMessage("add activity failed");
+            return Nav.FAIL;
+        }
     }
 
-    public Nav delete(Long id) {
+    public Nav delete(Post p) {
         System.out.println("Activity delete");
-        return _bo.delete(id) ? Nav.actitity_delete : Nav.FAIL;
+        if (_bo.delete(p)) {
+            return Nav.actitity_delete;
+        } else {
+            ThreadLocals.setErrorMessage("delete activity failed");
+            return Nav.FAIL;
+        }
     }
 
-    public Nav edit(Long id) {
-        return null;
+    public Nav edit(Post p) {
+        ThreadLocals.setPostToEdit(p);
+        return Nav.activity_edit;
     }
 
-    public Boolean isOwner(Long id) {
-        return _bo.isOwner(id);
+    public Boolean isOwner(Post p) {
+        return _bo.isOwner(p);
     }
 
-    public Collection<Post> getAll(boolean isPublic) {
-        return _bo.getAll(isPublic);
+    public Collection<Post> getAllByCommunity(Community c) {
+        return c.getPosts();
+    }
+
+    public Collection<Post> getAllByUser() {
+        return _bo.getAllByUser();
     }
 
     public String getEntry() {
@@ -43,13 +58,5 @@ public class ActivityPostBean {
 
     public void setEntry(String entry) {
         this.entry = entry;
-    }
-
-    public boolean getIsPublic() {
-        return isPublic;
-    }
-
-    public void setIsPublic(boolean isPublic) {
-        this.isPublic = isPublic;
     }
 }
