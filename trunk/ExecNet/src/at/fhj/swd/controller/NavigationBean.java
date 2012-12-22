1c56d5b4c71a45c50b96199317aca5f464401965
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-12-22 11:35:33 +0000

    Added Navigation Bean

---
+++ trunk/ExecNet/src/at/fhj/swd/controller/NavigationBean.java
@@ -0,0 +1,32 @@
+package at.fhj.swd.controller;
+
+/**
+ * @author david.tomes
+ */
+public class NavigationBean {
+
+    public String home() {
+        return "new_home";
+    }
+
+    public String myPage() {
+        return "new_myPage";
+    }
+
+    public String users() {
+        return "new_users";
+    }
+
+    public String communities() {
+        return "new_communities_management";
+    }
+
+    public String documents() {
+        return "new_documents";
+    }
+
+    public String settings() {
+        return "new_settings";
+    }
+
+}
