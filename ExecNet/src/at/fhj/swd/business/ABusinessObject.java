ea3cd422e6f9b6780f6c62ac7e661269ecd88159
david.tomes21 <david.tomes21@gmail.com>
2012-12-22 12:48:44 +0000

    Added Logs to UserBO

--- ExecNet/src/at/fhj/swd/business/ABusinessObject.java
+++ ExecNet/src/at/fhj/swd/business/ABusinessObject.java
@@ -5,24 +5,21 @@ import org.apache.log4j.Logger;
 import at.fhj.swd.application.Application;
 import at.fhj.swd.application.IRuntimeContext;
 
-
 public class ABusinessObject {
-	  
-	private IRuntimeContext _rt;
-	protected static final Logger logger = Logger.getLogger("BusinessLayer");
 
+    protected static final Logger logger = Logger.getLogger("BusinessLayer");
 
-	public IRuntimeContext getRuntimeContext() {
-		return _rt;
-	}
+    private IRuntimeContext _rt;
 
-	public void setRuntimeContext(IRuntimeContext _rt) {
-		this._rt = _rt;
-	}
-	
-	public ABusinessObject (){
-		this._rt = Application.getInstance().getRuntime();
-	}
-	
-	  
+    public ABusinessObject() {
+        this._rt = Application.getInstance().getRuntime();
+    }
+
+    public IRuntimeContext getRuntimeContext() {
+        return _rt;
+    }
+
+    public void setRuntimeContext(IRuntimeContext _rt) {
+        this._rt = _rt;
+    }
 }
---
+++ ExecNet/test/at/fhj/swd/business/UserBOTest.java
@@ -0,0 +1,8 @@
+package at.fhj.swd.business;
+
+/**
+ * @author david.tomes
+ */
+public class UserBOTest {
+
+}
