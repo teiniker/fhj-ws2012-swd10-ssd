42d9471883de61e01a2f64c0088bdc46a4e1fb4d
hausegger.michael <hausegger.michael@gmail.com>
2012-12-29 21:40:13 +0000

    US 18, Team CAFK, SST file upload improvements

---
+++ ExecNet/src/at/fhj/swd/business/sstFile/SSTFile.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package at.fhj.swd.business.sstFile;
+
+/**
+ * @author Michael Hausegger, CAFK
+ * 
+ */
+public class SSTFile {
+
+    // Haui noch überlegen ob du diese Klasse wirklich brauchst.
+
+}
