6c333db4188b5ba9ccd73a6c55cbc69328dae0aa
hausegger.michael@gmail.com <hausegger.michael@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2012-12-29 16:37:35 +0000

    US 18, Team CAFK, SST file upload improvements

--- trunk/ExecNet/src/at/fhj/swd/controller/SSTFileBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/SSTFileBean.java
@@ -21,7 +21,16 @@ public class SSTFileBean {
 	FacesMessage _msg = new FacesMessage("Succesful", event.getFile()
 		.getFileName() + " is uploaded.");
 
-	FacesContext.getCurrentInstance().addMessage(null, _msg);
+	if (FacesContext.getCurrentInstance().getMessages("message_sst_file")
+		.hasNext())
+	    FacesContext.getCurrentInstance().getMessages("message_sst_file")
+		    .remove();
+
+	FacesContext.getCurrentInstance().addMessage("message_sst_file", _msg);
+
+	// Haui hier später noch überlegen wegen der Anzeige der automatischen
+	// Fehlermessages.
+	// Jedoch: http://forum.primefaces.org/viewtopic.php?f=3&t=23853
 
     }
 
