47a9b8b851633fb86cc53b3930946a7bddd2da8e
hausegger.michael@gmail.com <hausegger.michael@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-01-02 21:32:26 +0000

    US 18, Team CAFK, SST file upload improvements

---
+++ trunk/ExecNet/test/at/fhj/swd/utils/sst/SSTFileDataExtractorResultTest.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package at.fhj.swd.utils.sst;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import at.fhj.swd.domain.User;
+
+/**
+ * @author haui
+ * 
+ */
+public class SSTFileDataExtractorResultTest {
+
+    private final SSTFileDataExtractorResult _goodResult = new SSTFileDataExtractorResult();
+
+    private final SSTFileDataExtractorResult _badResult = new SSTFileDataExtractorResult();
+
+    private static final String errorMessageString = new String(
+	    "Bad event occured");
+
+    /**
+     * Constructs the two classes that will be tested. A good result and a bad
+     * result.
+     * 
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+	testSetParsingSuccessfull();
+
+	testSetErrorMessage();
+
+	testSetUsers();
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#getParsingSuccessfull()}
+     * .
+     */
+    @Test
+    public void testGetParsingSuccessfull() {
+
+	assertEquals(true, this._goodResult.getParsingSuccessfull()
+		.booleanValue());
+
+	assertEquals(false, this._badResult.getParsingSuccessfull()
+		.booleanValue());
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#setParsingSuccessfull(java.lang.Boolean)}
+     * .
+     */
+    @Test
+    public void testSetParsingSuccessfull() {
+
+	this._goodResult.setParsingSuccessfull(new Boolean(true));
+	this._badResult.setParsingSuccessfull(new Boolean(false));
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#getErrorMessage()}
+     * .
+     */
+    @Test
+    public void testGetErrorMessage() {
+
+	assertEquals(this.errorMessageString, this._badResult.getErrorMessage());
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#setErrorMessage(java.lang.String)}
+     * .
+     */
+    @Test
+    public void testSetErrorMessage() {
+
+	this._badResult.setErrorMessage(this.errorMessageString);
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#getUsers()}.
+     */
+    @Test
+    public void testGetUsers() {
+
+	User _user = this._goodResult.users.get(0);
+
+	assertEquals(1, this._goodResult.users.size());
+
+	assertEquals("firstname", _user.getFirstname());
+
+	assertEquals("lastname", _user.getLastname());
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#setUsers(java.util.List)}
+     * .
+     */
+    @Test
+    public void testSetUsers() {
+
+	User _user = new User();
+
+	List<User> _arrayListWithUser = new ArrayList<User>();
+
+	_user.setFirstname("firstname");
+	_user.setLastname("lastname");
+
+	_arrayListWithUser.add(_user);
+
+	this._goodResult.setUsers(_arrayListWithUser);
+
+	testGetUsers();
+
+    }
+
+    /**
+     * Test method for
+     * {@link at.fhj.swd.utils.sst.SSTFileDataExtractorResult#addUser(at.fhj.swd.domain.User)}
+     * .
+     */
+    @Test
+    public void testAddUser() {
+
+	User _user = new User();
+
+	_user.setFirstname("firstname");
+	_user.setLastname("lastname");
+
+	this._goodResult.users.clear();
+
+	this._goodResult.addUser(_user);
+
+	testGetUsers();
+
+    }
+
+}
