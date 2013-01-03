91da30fb7f1eeb3603f30601f580c6bfab8e35fc
david.tomes21 <david.tomes21@gmail.com>
2013-01-03 11:55:08 +0000

    Added Post Construct for Community Bean, so you can now navigate from Profile Page to one of the communities (overview page)!

--- ExecNet/src/at/fhj/swd/business/CommunityBO.java
+++ ExecNet/src/at/fhj/swd/business/CommunityBO.java
@@ -9,61 +9,61 @@ import at.fhj.swd.domain.User;
 
 public class CommunityBO extends ABusinessObject {
 
-	private IDataContext<Community> _context;
-	private IDataContext<User> _usercontext;
-	
-	public CommunityBO(){
-		super();
-		this._context = Application.getInstance().getCommunityContext();
-		this._usercontext = Application.getInstance().getUserContext();
-	}
-	
-    public boolean add(Community community) {
-    	try {
-	    	User _u = this.getRuntimeContext().getCurrentUser();
-	        community.setAdmin(_u);
-	        community.addUser(_u);
-	        _u.addCommunity(community);
+    private IDataContext<Community> _context;
+    private IDataContext<User> _usercontext;
 
-	        if (_context.create(community)) {
+    public CommunityBO() {
+        super();
+        this._context = Application.getInstance().getCommunityContext();
+        this._usercontext = Application.getInstance().getUserContext();
+    }
+
+    public boolean add(Community community) {
+        try {
+            User _u = this.getRuntimeContext().getCurrentUser();
+            community.setAdmin(_u);
+            community.addUser(_u);
+            _u.addCommunity(community);
+
+            if (_context.create(community)) {
                 this.getRuntimeContext().setAuthenticated(_usercontext.update(_u));
                 return true;
             } else {
                 return false;
             }
         } catch (Exception e) {
-        	logger.error(e);
+            logger.error(e);
             return false;
         }
     }
 
     public boolean join(Long id) {
-    	try{
-		    User _u = this.getRuntimeContext().getCurrentUser();
-		    Community c = _context.readOne(id, Community.class);
-		    c.addUser(_u);
-		    _context.update(c);
-		    _u.addCommunity(c);
-		    _usercontext.update(_u);
-		    return true;
-    	} catch (Exception e) {
-    		logger.error(e);
-    		return false;
-    	}
+        try {
+            User _u = this.getRuntimeContext().getCurrentUser();
+            Community c = _context.readOne(id, Community.class);
+            c.addUser(_u);
+            _context.update(c);
+            _u.addCommunity(c);
+            _usercontext.update(_u);
+            return true;
+        } catch (Exception e) {
+            logger.error(e);
+            return false;
+        }
     }
 
     public boolean leave(Long id) {
         try {
-        	User _u = this.getRuntimeContext().getCurrentUser();
-	        Community c = _context.readOne(id, Community.class);
-	        c.removeUser(_u);
-	        _context.update(c);
-	        _u.removeCommunity(c);
-	        _usercontext.update(_u);
-	        return true;
-        } catch (Exception e){
-        	logger.error(e);
-        	return false;
+            User _u = this.getRuntimeContext().getCurrentUser();
+            Community c = _context.readOne(id, Community.class);
+            c.removeUser(_u);
+            _context.update(c);
+            _u.removeCommunity(c);
+            _usercontext.update(_u);
+            return true;
+        } catch (Exception e) {
+            logger.error(e);
+            return false;
         }
     }
 
@@ -84,7 +84,7 @@ public class CommunityBO extends ABusinessObject {
             return (_context.delete(c));
         } catch (Exception e) {
             logger.error(e);
-        	return false;
+            return false;
         }
     }
 
@@ -92,18 +92,28 @@ public class CommunityBO extends ABusinessObject {
         try {
             return _context.readAll(Community.class);
         } catch (Exception e) {
-        	logger.error(e);
+            logger.error(e);
             return null;
         }
     }
 
     public Collection<Community> getMy() {
         try {
-        	User _u = this.getRuntimeContext().getCurrentUser();
-        	return _u.getCommunities();
+            User _u = this.getRuntimeContext().getCurrentUser();
+            return _u.getCommunities();
         } catch (Exception e) {
-        	logger.error(e);
-        	return null;
+            logger.error(e);
+            return null;
+        }
+    }
+
+    public Community getOne(Long id) {
+        try {
+            Community c = _context.readOne(id, Community.class);
+            return c;
+        } catch (Exception e) {
+            logger.error(e);
+            return null;
         }
     }
 
