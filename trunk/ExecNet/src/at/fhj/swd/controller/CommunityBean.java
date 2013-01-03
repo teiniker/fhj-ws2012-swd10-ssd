522b958815d25b99d88be28d36cb3d39ee68424e
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-01-03 11:55:08 +0000

    Added Post Construct for Community Bean, so you can now navigate from Profile Page to one of the communities (overview page)!

--- trunk/ExecNet/src/at/fhj/swd/controller/CommunityBean.java
+++ trunk/ExecNet/src/at/fhj/swd/controller/CommunityBean.java
@@ -2,6 +2,10 @@ package at.fhj.swd.controller;
 
 import java.util.Collection;
 
+import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import at.fhj.swd.business.CommunityBO;
 import at.fhj.swd.domain.Community;
 
@@ -9,15 +13,27 @@ public class CommunityBean {
 
     private String name;
     private String description;
-    
+    private Community viewCommunity;
+
     private CommunityBO _bo;
-    
+
     public CommunityBean() {
-    	_bo = new CommunityBO();
+        _bo = new CommunityBO();
+    }
+
+    @PostConstruct
+    public void init() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+        try {
+            Long id = Long.valueOf(context.getRequestParameterMap().get("viewCommunityId"));
+            viewCommunity = _bo.getOne(id);
+        } catch (Exception e) {
+            viewCommunity = null;
+        }
     }
 
     public String addNow() {
-        
         Community _new = new Community();
         _new.setDescription(this.getDescription());
         _new.setName(this.getName());
@@ -30,22 +46,22 @@ public class CommunityBean {
 
     public String joinNow(Long id) {
         if (_bo.join(id))
-        	return "user-joined";
+            return "user-joined";
         return "user-join-failed";
     }
 
     public String leaveNow(Long id) {
-       if (_bo.leave(id))
-    	   return "user-left";
-       return "user-leave-failed";
+        if (_bo.leave(id))
+            return "user-left";
+        return "user-leave-failed";
     }
 
     public boolean isCurrentMember(Long id) {
-    	return _bo.isCurrentMember(id);
+        return _bo.isCurrentMember(id);
     }
 
     public Boolean delete(Long id) {
-    	return _bo.delete(id);
+        return _bo.delete(id);
     }
 
     public Collection<Community> getAll() {
@@ -70,5 +86,13 @@ public class CommunityBean {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Community getViewCommunity() {
+        return viewCommunity;
+    }
+
+    public void setViewCommunity(Community viewCommunity) {
+        this.viewCommunity = viewCommunity;
     }
 }
