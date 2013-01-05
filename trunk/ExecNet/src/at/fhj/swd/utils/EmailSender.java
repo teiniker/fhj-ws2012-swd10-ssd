51241764b84db669f3b5d9c8f7a54c2162defbec
david.tomes21@gmail.com <david.tomes21@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-01-05 11:13:13 +0000

    Code Formatting

--- trunk/ExecNet/src/at/fhj/swd/utils/EmailSender.java
+++ trunk/ExecNet/src/at/fhj/swd/utils/EmailSender.java
@@ -13,41 +13,39 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class EmailSender {
-	String _recipient;
-	String _subject;
-	String _message;
-	String _emailuser;
-	String _sender;
-	String _username;
-	Authenticator _authenticator;
-	Properties _properties;
 
-	public EmailSender(String recipient, String subject, String message,
-			String emailuser, String sender, String username,
-			Authenticator authenticator, Properties properties) {
-		this._recipient = recipient;
-		this._subject = subject;
-		this._message = message;
-		this._emailuser = emailuser;
-		this._sender = sender;
-		this._username = username;
-		this._authenticator = authenticator;
-		this._properties = properties;
-	}
+    String _recipient;
+    String _subject;
+    String _message;
+    String _emailuser;
+    String _sender;
+    String _username;
+    Authenticator _authenticator;
+    Properties _properties;
 
-	public void Send() throws AddressException, MessagingException {
-		Session _session = Session.getDefaultInstance(_properties,
-				_authenticator);
-		Address[] address = new InternetAddress[1];
-		address[0] = new InternetAddress(_sender);
-		MimeMessage message = new MimeMessage(_session);
-		message.setFrom(new InternetAddress(_emailuser));
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(
-				_recipient));
-		message.setReplyTo(address);
-		message.setSubject("ExecNet-Mail " + _username + ": " + _subject);
-		message.setText(_message);
-		Transport.send(message);
-	}
+    public EmailSender(String recipient, String subject, String message, String emailuser, String sender,
+            String username, Authenticator authenticator, Properties properties) {
+        this._recipient = recipient;
+        this._subject = subject;
+        this._message = message;
+        this._emailuser = emailuser;
+        this._sender = sender;
+        this._username = username;
+        this._authenticator = authenticator;
+        this._properties = properties;
+    }
+
+    public void Send() throws AddressException, MessagingException {
+        Session _session = Session.getDefaultInstance(_properties, _authenticator);
+        Address[] address = new InternetAddress[1];
+        address[0] = new InternetAddress(_sender);
+        MimeMessage message = new MimeMessage(_session);
+        message.setFrom(new InternetAddress(_emailuser));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(_recipient));
+        message.setReplyTo(address);
+        message.setSubject("ExecNet-Mail " + _username + ": " + _subject);
+        message.setText(_message);
+        Transport.send(message);
+    }
 
 }
