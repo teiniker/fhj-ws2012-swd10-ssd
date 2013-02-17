d4b15cd484cb4b926690070e821bb54b00b477cf
jan.moser.swd10@gmail.com <jan.moser.swd10@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-02-17 16:22:01 +0000

    Moser Jan - Added Document Upload Test 		  - Added DocumentCommunity Selection Test

---
+++ trunk/ExecNet/test/at/fhj/swd/selenium/test/DocumentsTest.java
@@ -0,0 +1,36 @@
+package at.fhj.swd.selenium.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import at.fhj.swd.selenium.AbstractTest;
+import at.fhj.swd.selenium.pages.Documents;
+
+public class DocumentsTest extends AbstractTest {
+
+
+    @Test
+    public void uploadDokument() throws Exception {
+        Documents documentsPage = super.page.clickDocuments();
+        String documentName = "Desert.jpg";
+
+        int documentsCountBefore = documentsPage.getDocumentsCount();
+
+        String lastDocument = documentsPage.uploadDocument(documentName);
+
+        int documentsCountAfter = documentsPage.getDocumentsCount();
+
+        Assert.assertTrue(documentsCountAfter > documentsCountBefore);
+        Assert.assertEquals(documentName, lastDocument);
+    }
+
+    @Test
+    public void selectDokumentCommunity() throws Exception {
+        Documents documentsPage = super.page.clickDocuments();
+        int pos = 1;
+        documentsPage.selectDocumentCommunity(pos);
+
+        Assert.assertTrue(documentsPage.isDocumentCommunitySelected(pos));
+    }
+}
