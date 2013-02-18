02dc5599a18d786e54684894db27d1ed05362979
georgmoik <georgmoik@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-02-18 19:51:27 +0000

    MoikGeorg SileniumTest

--- trunk/ExecNet/test/at/fhj/swd/selenium/test/ActivityTest.java
+++ trunk/ExecNet/test/at/fhj/swd/selenium/test/ActivityTest.java
@@ -20,7 +20,15 @@ public class ActivityTest extends AbstractTest {
         String testtext = driver.findElement(
             By.xpath("html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[6]/td/ul/li")).getText()
             .substring(8);
-        Assert.assertEquals(testtext, ":entry: Überprüfungsfehler: Wert ist erforderlich.");
+        Assert.assertEquals(testtext, ":entry: ï¿½berprï¿½fungsfehler: Wert ist erforderlich.");
+    }
 
+    @Test
+    public void testifDataIsInList() {
+        String sSource = "Text text";
+        page.homepage().fillActivity(sSource);
+        String sDest = driver.findElement(
+            By.xpath("html/body/div/table[3]/tbody/tr/td[2]/div/div/form[1]/table/tbody/tr[2]/td[2]/strong")).getText();
+        Assert.assertEquals(sSource, sDest);
     }
 }
