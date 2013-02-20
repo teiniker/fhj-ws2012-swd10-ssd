1b81330414ad5e7201379d1b1ef3fb59ed015dc2
mkronberger79@gmail.com <mkronberger79@gmail.com@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-02-20 14:16:03 +0000

    Added Selenium Test for Pinboard US.

---
+++ trunk/ExecNet/test/at/fhj/swd/selenium/test/PinboardTest.java
@@ -0,0 +1,17 @@
+package at.fhj.swd.selenium.test;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+import at.fhj.swd.selenium.AbstractTest;
+import at.fhj.swd.selenium.pages.MySite;
+
+public class PinboardTest extends AbstractTest {
+
+    @Test
+    public void insertPinBoardEntry() {
+        MySite mySite = page.clickMySide();
+        Assert.assertEquals("TEST", mySite.insertPinBoardEntry("TEST"));
+    }
+
+}
