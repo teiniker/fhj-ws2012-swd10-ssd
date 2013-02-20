f9265f3f197942619763daa01e127722ec9f81cc
hausegger.michael <hausegger.michael@gmail.com>
2013-02-20 18:49:17 +0000

    Added possibility to change username and password of test in class AbstractTest.java

--- ExecNet/test/at/fhj/swd/selenium/AbstractTest.java
+++ ExecNet/test/at/fhj/swd/selenium/AbstractTest.java
@@ -7,23 +7,55 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import at.fhj.swd.selenium.pages.Pages;
 
-
 public abstract class AbstractTest {
 
     protected WebDriver driver;
     protected Pages page;
 
+    private String username = "TestNamen";
+    private String password = "TestPasswort";
+
     @Before
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        page = new Pages(driver);
-        page.maximizeWindow();
-        page.openSite("http://localhost:8080/ExecNet/");
-        page.loginPage().login("TestNamen", "TestPasswort");
+	driver = new FirefoxDriver();
+	page = new Pages(driver);
+	page.maximizeWindow();
+	page.openSite("http://localhost:8080/ExecNet/");
+	page.loginPage().login(this.username, this.password);
+    }
+
+    /**
+     * @return the username
+     */
+    private String getUsername() {
+	return username;
+    }
+
+    /**
+     * @param username
+     *            the username to set
+     */
+    private void setUsername(String username) {
+	this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    private String getPassword() {
+	return password;
+    }
+
+    /**
+     * @param password
+     *            the password to set
+     */
+    private void setPassword(String password) {
+	this.password = password;
     }
 
     @After
     public void tearDown() throws Exception {
-        driver.close();
+	driver.close();
     }
 }
