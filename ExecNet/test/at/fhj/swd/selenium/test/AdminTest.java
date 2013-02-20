60e46f7c5b04863f1521e96590148ad0e55ca6d0
hausegger.michael <hausegger.michael@gmail.com>
2013-02-20 18:40:52 +0000

    Added Selenium tests for setting and unsetting various admin roles to one specific user

--- ExecNet/test/at/fhj/swd/selenium/test/AdminTest.java
+++ ExecNet/test/at/fhj/swd/selenium/test/AdminTest.java
@@ -1,26 +1,60 @@
 package at.fhj.swd.selenium.test;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import at.fhj.swd.selenium.AbstractTest;
 
 /**
- * @author
+ * @author Michael Hausegger
  * 
  */
 public class AdminTest extends AbstractTest {
 
     @Test
-    public void test() {
+    public void isAdmin() {
 
 	page.clickAdmin();
 
-	try {
-	    Thread.sleep(10000);
-	} catch (InterruptedException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
+	WebElement button = driver.findElement(By
+		.name("listingTableId:j_idt63:0:j_idt66"));
+
+	// If the user is an admin this thest method raises no error.
+
+    }
+
+    @Test
+    public void testPortalAdminSettable() {
+
+	page.clickAdmin();
+
+	WebElement buttonPortalAdminSet = driver.findElement(By
+		.name("listingTableId:j_idt63:0:j_idt73"));
+
+	buttonPortalAdminSet.click();
+
+	WebElement buttonPortalAdminUnSet = driver.findElement(By
+		.name("listingTableId:j_idt63:0:j_idt71"));
+
+	buttonPortalAdminUnSet.click();
+
+    }
+
+    @Test
+    public void testDeactivateActivateUser() {
+
+	page.clickAdmin();
+
+	WebElement buttonDeactivateUser = driver.findElement(By
+		.name("listingTableId:j_idt63:0:j_idt76"));
+
+	buttonDeactivateUser.click();
+
+	WebElement buttonActivateUser = driver.findElement(By
+		.name("listingTableId:j_idt63:0:j_idt78"));
+
+	buttonActivateUser.click();
 
     }
 
