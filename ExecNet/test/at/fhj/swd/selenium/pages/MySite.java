d9e05024e516c854ad32e1b8c3ced18e6250966e
mkronberger79 <mkronberger79@gmail.com>
2013-02-20 14:16:03 +0000

    Added Selenium Test for Pinboard US.

--- ExecNet/test/at/fhj/swd/selenium/pages/MySite.java
+++ ExecNet/test/at/fhj/swd/selenium/pages/MySite.java
@@ -1,16 +1,26 @@
 package at.fhj.swd.selenium.pages;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import at.fhj.swd.selenium.AbstractPage;
 
+public class MySite extends AbstractPage {
 
-public class MySite extends AbstractPage{
-    
     public MySite(WebDriver driver) {
         super(driver);
-        
     }
 
- 
+    public String insertPinBoardEntry(String entry) {
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div/table[3]/tbody/tr/td/form/table/tbody/tr/td[2]/input")).sendKeys(entry);
+        driver.findElement(By.xpath("/html/body/div/table[3]/tbody/tr/td/form/input[2]")).click();
+        
+        WebElement element = driver.findElement(By.xpath("/html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr/td[2]/span"));
+        return element.getText();
+    }
+
 }
