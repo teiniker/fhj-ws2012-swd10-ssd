4cdef0ab1d99436f99f7e146e522f375f65b8fbb
scotfox.SF81 <scotfox.SF81@d6f2b6a1-3553-0423-96d1-a6d91f4ed79a>
2013-02-24 14:48:01 +0000

    Xpath angepasst und Monatsweiterschaltung implementiert

--- trunk/ExecNet/test/at/fhj/swd/selenium/pages/Homepage.java
+++ trunk/ExecNet/test/at/fhj/swd/selenium/pages/Homepage.java
@@ -9,22 +9,25 @@ import at.fhj.swd.selenium.AbstractPage;
 
 public class Homepage extends AbstractPage {
 
-    @FindBy(xpath = "html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[2]/textarea")
+    @FindBy(xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[2]/td[2]/textarea")
     private WebElement entryField;
     @FindBy(xpath = "html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr[5]/td[1]/select")
     private WebElement dropdownboxCommunity;
     @FindBy(xpath = "html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr[5]/td[2]/input")
     private WebElement btnCreateActivity;
-    @FindBy(xpath = "html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr[3]/td[2]/div/span/span[1]/img")
+    @FindBy(xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[3]/td[2]/div/span/span[1]/img")
     private WebElement btnPopupDateFrom;
     @FindBy(
             xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[3]/td[2]/div/span/table/tbody/tr[4]/td[3]")
     private WebElement btnDateFrom;
-    @FindBy(xpath = "html/body/div/table[3]/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/div/span/span[1]/img")
+    @FindBy(xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/div/span/span[1]/img")
     private WebElement btnPopupDateTo;
     @FindBy(
             xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/div/span/table/tbody/tr[7]/td[7]")
     private WebElement btnDateTo;
+    @FindBy(
+            xpath = "html/body/div[1]/table[3]/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/div/span/table/tbody/tr[1]/td/table/tbody/tr/td[4]/div")
+    private WebElement btnNextMonth;
 
     public Homepage(WebDriver driver) {
         super(driver);
@@ -36,6 +39,7 @@ public class Homepage extends AbstractPage {
         btnPopupDateFrom.click();
         btnDateFrom.click();
         btnPopupDateTo.click();
+        btnNextMonth.click();
         btnDateTo.click();
         sendActivity();
     }
